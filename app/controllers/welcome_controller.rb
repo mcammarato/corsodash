ec1c72c09a2900432bf001b7db49f14e228df7c2
mcammarato <mike@Mikes-MacBook-Pro.local>
2014-11-24 16:41:30 -0600

    changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
 	before_filter :authenticate_user!
+	respond_to :html
   def index
-  	  	
+  	  	@tasks = Task.all
+		 @discussions = Discussion.all
   end
 end
