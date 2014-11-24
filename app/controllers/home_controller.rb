872b67975fc0f597bda16490379e39333ae8b676
mcammarato <mike@Mikes-MacBook-Pro.local>
2014-11-24 16:24:59 -0600

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 	before_filter :authenticate_user!
 	def index
 		@tasks = Task.all
-		@discussions = Discussion.all
+		 @discussions = Discussion.all
     		#render :layout => "homelayout"
 	end
 	
