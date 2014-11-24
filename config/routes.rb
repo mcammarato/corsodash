e09800652d86104b59d104be34f13b844425882d
mcammarato <mike@Mikes-MacBook-Pro.local>
2014-11-24 16:13:18 -0600

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,13 @@ Rails.application.routes.draw do
 
   #devise_for :user
   resources :clients
-   get "home/index"
+  
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root to: 'home#index'
 
 
 
